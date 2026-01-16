7c662c4bb072cec5c65e5e57983e28f503d199ef
Arunjeet <85785543+Arunjeet@users.noreply.github.com>
2026-01-16 11:12:57 +0000

    Update file paths and sheet names in main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from transformations import load_client_raw
 def main():
     ensure_schema()
     inserted = load_bank_raw(
-        path=r"C:\ProgramData\Reconciliation_design\Nonco_LLC_-_Bank_Reconciliation.xlsx",
-        sheet="Nonco LLC - xxx2006 Reconcil...",
+        path=r"C:\ProgramData\Reconciliation_design\path.xlsx",
+        sheet="bankname Reconcil...",
         cols=['date','unnamed1','transaction','amount']
     )
     print(f"Inserted rows bank raw: {inserted}")
@@ -21,3 +21,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
